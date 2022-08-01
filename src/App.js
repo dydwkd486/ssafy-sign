681a5cea5519e8eed44a89e3a95ba99784d80b71
yongjang cho <dydwkd48670@gmail.com>
2022-08-01 22:54:54 +0900

    update

--- src/App.js
+++ src/App.js
@@ -11,14 +11,18 @@ function App() {
       <div className="header">
         <img src={logo} className="App-logo" alt="logo" height="100px" width="100px" />
         <h1>SSAFY 교육지원금 서명 생성기</h1>
+        <p>8기 버전</p>
       </div>
 
       <Container />
 
       <div className="footer">
-        <p> 8기에 맞게 수정 버전입니다.</p>
-        {/* <p>&copy;  SSAFY 8기 서울캠퍼스 조용장</p>
-        <p>문의사항 / 버그 : dydwkd48670@gmail.com</p> */}
+        
+        <p>&copy;  SSAFY 5기 서울캠퍼스 최동욱</p>
+        <p>문의사항 / 버그 : cheal3@naver.com</p>        
+
+        <p>수정자 : SSAFY 8기 서울캠퍼스 조용장</p>
+
       </div>
 
 
